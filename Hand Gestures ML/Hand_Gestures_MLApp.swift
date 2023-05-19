bd5735dbce4114a48c779829f5b71f16edc602c3
saepasomba <saepasomba12@gmail.com>
2023-05-19 16:18:29 +0700

    Initial Commit

---
+++ Hand Gestures ML/Hand_Gestures_MLApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hand_Gestures_MLApp.swift
+//  Hand Gestures ML
+//
+//  Created by Sae Pasomba on 19/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Hand_Gestures_MLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
